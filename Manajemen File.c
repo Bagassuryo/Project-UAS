d33b00657dd1b53fce1878beff12284975c8a3f3
Thareeqzrd <143929917+ThareeqZiadRamadhan@users.noreply.github.com>
2024-12-21 16:13:02 +0700

    Add files via upload

---
+++ Manajemen File.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define MAX_MENU 50
+#define MAX_NAME 50
+#define ADMIN_PASS "admin123"
+
+//program file untuk menyimpan menu kedalam file dokumen
+void saveMenuToFile(Menu menu[], int n, const char *filename) {
+    FILE *file = fopen(filename, "w");
+    if (!file) {
+        printf("Gagal menyimpan ke file %s.\n", filename);
+        return;
+    }
+    int i;
+    for (i = 0; i < n; i++) {
+      fprintf(file, "%s,%.2f,%d,%.2f,%d\n", menu[i].name, menu[i].price, menu[i].stock, menu[i].rating, menu[i].ratingCount);
+    }
+    fclose(file);
+}
+
+//fitur untuk memuat menu dari file dokumen
+void loadMenuFromFile(Menu menu[], int *n, const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        printf("File %s tidak ditemukan.\n", filename);
+        return;
+    }
+    char line[100];
+    *n = 0;
+    while (fgets(line, sizeof(line), file)) {
+       sscanf(line, "%[^,],%lf,%d,%lf,%d", menu[*n].name, &menu[*n].price, &menu[*n].stock, &menu[*n].rating, &menu[*n].ratingCount); 
+        (*n)++;
+    }
+    fclose(file);
+}
+
